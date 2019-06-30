3e2fb3ff24088df78c0fd9ab5e337c1e9f89f9bf
muhwezi deo <aggrey256@gmail.com>
2019-06-30 18:37:30 +0300

    setup angular material

---
+++ client/src/app/angular-material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule, MatCardModule} from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [MatToolbarModule, MatCardModule],
+  exports: [MatToolbarModule, MatCardModule]
+})
+export class AngularMaterialModule { }
