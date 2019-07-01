011771ed93b3ed524361fc3e4114bb5bbbf88730
muhwezi deo <aggrey256@gmail.com>
2019-07-01 11:03:43 +0300

    setup navigation bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AngularMaterialModule } from './angular-material.module';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AngularMaterialModule],
+    AngularMaterialModule,
+    LayoutModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
